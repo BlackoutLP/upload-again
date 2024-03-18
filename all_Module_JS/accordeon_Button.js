808b9cb4385f032975b7b27df4d1b97d7779276a
BlackoutLP <godofwar.lion@gmail.com>
2024-03-17 18:30:31 -0600

    modificando el accordion

--- all_Module_JS/accordeon_Button.js
+++ all_Module_JS/accordeon_Button.js
@@ -1,35 +1,17 @@
 export default function accordeonBtn() {
-  const $btnAccordeon = document.querySelectorAll(".btnAccordion");
-  const $cardAccordion = document.querySelectorAll(".cardAccordion");
+  const items = document.querySelectorAll(".container-accordion button");
 
-  for (let i = 0; i < $btnAccordeon.length; i++) {
-    const el = $btnAccordeon[i];
-    el.addEventListener("click", (e) => {
-      if (e.target) {
-        $cardAccordion.forEach((el) => {
-          el.classList.add("active_accordeon");
-        });
-      }
-    });
+  function toggleAccordion() {
+    const itemToggle = this.getAttribute("aria-expanded");
+
+    for (let i = 0; i < items.length; i++) {
+      items[i].setAttribute("aria-expanded", "false");
+    }
+
+    if (itemToggle == "false") {
+      this.setAttribute("aria-expanded", "true");
+    }
   }
 
-  document.addEventListener("click", (e) => {});
-  var acc = document.getElementsByClassName("btnAccordion");
-  var $panel = document.getElementsByClassName("cardAccordion");
-
-  // console.log(acc);
-  // console.log($panel);
-  // for (let i = 0; i < acc.length; i++) {
-  //   acc[i].addEventListener("click", function () {
-  //     this.classList.toggle("active_accordeon");
-
-  //     var panel = this.nextElementSibling;
-  //     if (panel.style.display === "block") {
-  //       $panel.classList.remove("active_accordeon");
-  //       $panel.style.display = "none";
-  //     } else {
-  //       panel.style.display = "block";
-  //     }
-  //   });
-  // }
+  items.forEach((item) => item.addEventListener("click", toggleAccordion));
 }
